182d5f05031da03c90ad4eff67fa1a2269b095fe
ChenAnci <1062492633@qq.com>
2026-09-17 22:34:17 +0800

    feat(chat): 新增聊天 mapper

---
+++ ruoyi-market/src/main/java/com/ruoyi/market/mapper/MarketChatMessageMapper.java
@@ -0,0 +1,11 @@
+package com.ruoyi.market.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.market.domain.MarketChatMessage;
+
+/**
+ * 聊天消息 Mapper
+ */
+public interface MarketChatMessageMapper extends BaseMapper<MarketChatMessage>
+{
+}
---
+++ ruoyi-market/src/main/java/com/ruoyi/market/mapper/MarketChatSessionMapper.java
@@ -0,0 +1,11 @@
+package com.ruoyi.market.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.ruoyi.market.domain.MarketChatSession;
+
+/**
+ * 聊天会话 Mapper
+ */
+public interface MarketChatSessionMapper extends BaseMapper<MarketChatSession>
+{
+}
